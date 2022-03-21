86cff1449096606a8238e80368a8e159dfc4df44
Ilya Serhiyenka <91072080+P1l1gr1m@users.noreply.github.com>
2022-03-21 09:27:32 +0000

    Code edited

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
-/* hello.c */
 #include <stdio.h>
-
-void main (void)
+ 
+int main()
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+    char text[] = "fajsfjasjf asfasf asdasd asdasd.";
+    int slen = 0, count = 0, index = 0; 
+ 
+    for(slen = 0; text[slen]!='\0'; slen++){
+      
+    };
+    
+    for(int i = slen; i!=0; i--){
+        if( text[i] == ' ' ){ count++; };
+        if( count == 2 ){ index = i+1; break;};
+    };
+ 
+    for(int i = index; text[i]!=' '; i++){
+        printf("%c", text[i]);
+    };
+    printf("\n");
+    
+    return 0;
 }
